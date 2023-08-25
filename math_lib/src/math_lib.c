3f60e8a5d51d15042c25de349406820f96c7894f
Foiht <phojgt@.gmail.com>
2023-08-25 18:24:13 +0300

    added main func to test max

--- math_lib/src/math_lib.c
+++ math_lib/src/math_lib.c
@@ -2,7 +2,6 @@
 
 int max(int num1, int num2) {
 
-   /* local variable declaration */
    int result;
  
    if (num1 > num2)
@@ -11,4 +10,15 @@ int max(int num1, int num2) {
       result = num2;
  
    return result; 
+}
+
+int main() {
+
+   int a = 100;
+   int b = 200;
+   int ret;
+
+   ret = max(a, b);
+ 
+   printf( "Max value is : %d\n", ret );
 }
